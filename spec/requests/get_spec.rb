0dc6448e7c120190eb3dfafd11af24a140101d00
Kat Gifford <katgiff2007@gmail.com>
2017-07-21 12:41:28 -0700

    add rspec test for scopes

---
+++ spec/requests/get_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe "get all plants route", type: :request do
+  let!(:plants) { FactoryGirl.create_list(:plant, 20)}
+
+  before { get '/plants'}
+
+  it 'returns all plants' do
+     expect(jsonParse.size).to eq (20)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
+  it 'returns plants by name' do
+    plant = plants.first
+    get "/plants?name=#{plant.name}"
+    output = JSON.parse(response.body).first
+    expect(output.fetch("name")).to eq "#{plant.name}"
+  end
+
+  it 'returns plants by scientific name' do
+    plant = plants.first
+    get "/plants?scientific_name=#{plant.scientific_name}"
+    output = JSON.parse(response.body).first
+    expect(output.fetch("scientific_name")).to eq "#{plant.scientific_name}"
+  end
+
+  it 'returns plants by plant type' do
+    plant = plants.first
+    get "/plants?plant_type=#{plant.plant_type}"
+    output = JSON.parse(response.body).first
+    expect(output.fetch("plant_type")).to eq "#{plant.plant_type}"
+  end
+
+end
