4396a3a4128d4e64571e98d5ef25617daec3bf25
Kat Gifford <katgiff2007@gmail.com>
2017-07-21 11:19:30 -0700

    add spec for update

---
+++ spec/requests/update_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "update plant request", type: :request do
+  before do
+
+    plant = FactoryGirl.create(:plant)
+    put "/plants/#{plant.id}/?name=Willow"
+  end
+
+  it 'should return status 200' do
+    expect(response.status).to eq 200
+  end
+
+  it 'should return status 200' do
+    plant = Plant.first
+    expect(plant.name).to eq "Willow"
+  end
+
+end
